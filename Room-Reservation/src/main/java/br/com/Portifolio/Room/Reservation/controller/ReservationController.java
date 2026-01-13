f49e5beb502b9389fe98c1d8b87733ad74534b99
IgorSantanask8 <santanaigor60@outlook.com>
2026-01-12 21:30:55 -0300

    Mapping Change

--- Room-Reservation/src/main/java/br/com/Portifolio/Room/Reservation/controller/ReservationController.java
+++ Room-Reservation/src/main/java/br/com/Portifolio/Room/Reservation/controller/ReservationController.java
@@ -12,7 +12,7 @@ import java.io.IOException;
 import java.util.List;
 
 @RestController
-@RequestMapping("/hotel")
+@RequestMapping("/hotel/v1")
 public class ReservationController {
 
     @Autowired
